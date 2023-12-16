53daab92b86f7d631bdf6eb5139c053e66efd9e9
sander1946 <sanderkleine2@gmail.com>
2023-12-16 13:54:03 +0100

    Revert "needs to be undone"
    This reverts commit 1802ac26a76e637a4e3ff6b6e0720a97381aaf08.

--- app.py
+++ app.py
@@ -11,13 +11,7 @@ def read_global_code():
     f = open("code.txt", 'r')
     glob_code = f.read()
     f.close()
-    f = open("pref_code.txt", 'r')
-    pref_code = f.read()
-    f.close()
-    f = open("loop_id.txt", 'r')
-    loop_id = f.read()
-    f.close()
-    return glob_code, pref_code, loop_id
+    return glob_code
 
 
 def write_global_code(glob_code):
@@ -27,22 +21,8 @@ def write_global_code(glob_code):
     f.close()
 
 
-def write_pref_code(pref_code):
-    f = open("pref_code.txt", 'w')
-    print(pref_code)
-    f.write(pref_code)
-    f.close()
-
-
-def write_loop_id(loop_id):
-    f = open("loop_id.txt", 'w')
-    print(loop_id)
-    f.write(loop_id)
-    f.close()
-
-
 def connect_to_database():
-    connection = pymysql.connect(host='192.168.2.17',
+    connection = pymysql.connect(host='127.0.0.1',
                                  user='Hubo',
                                  password='Hubo2015',
                                  database="videos",
@@ -74,30 +54,21 @@ def drop_session():
 
 @app.route('/_get_video', methods=['GET'])
 def get_video():
-    code, pref_code, loop_id = read_global_code()
+    code = read_global_code()
     if code != 0:
         connection = connect_to_database()
         with connection:
             with connection.cursor() as cursor:
-                if code == pref_code:
-                    is_new = False
-                    if int(loop_id) >= 80:
-                        loop_id = 0
-                        write_loop_id(str(loop_id))
-                    loop_id = int(loop_id) + 1
-                    cursor.execute("SELECT * FROM `videos` WHERE `id` = %s",
-                                   [loop_id])
-                else:
-                    is_new = True
-                    write_pref_code(str(code))
-                    cursor.execute("SELECT * FROM `videos` WHERE `code` = %s",
-                                   [code])
+                cursor.execute("SELECT * FROM `videos` WHERE `code` = %s",
+                               [code])
                 vuurwerk_info = cursor.fetchone()
+                # naam = vuurwerk_info["naam"]
                 vid = vuurwerk_info["vid"]
+                # img = vuurwerk_info["img"]
                 start = vuurwerk_info["start"]
                 duration = vuurwerk_info["duration"]
         return jsonify(video=f"https://www.youtube-nocookie.com/embed/{vid}&start={start}&autoplay=1&mute=1",
-                       duration=duration, is_new=is_new)
+                       duration=duration)
     return jsonify(video="None", duration=2500)
 
 
